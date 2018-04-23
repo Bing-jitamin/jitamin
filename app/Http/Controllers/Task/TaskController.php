e946e31afe4a86582f6aa521842764654e2cf384
jian玄冰 <jianxuanhuo1@126.com>
2018-04-23 10:42:48 +0800

    移除 输出日志

--- app/Http/Controllers/Task/TaskController.php
+++ app/Http/Controllers/Task/TaskController.php
@@ -93,8 +93,7 @@ class TaskController extends Controller
         if ($sorting === 'date') {
             $filter->getQuery()->asc(TaskModel::TABLE.'.date_started')->asc(TaskModel::TABLE.'.date_creation');
         } else if ($sorting === 'namedate') {
-            $filter->getQuery()->asc(TaskModel::TABLE.'.owner_id')->asc(TaskModel::TABLE.'.date_started');
-            error_log('排序1',3,"G:\\Jitamin\\test.log");
+            $filter->getQuery()->asc(TaskModel::TABLE.'.owner_id')->asc(TaskModel::TABLE.'.date_started');            
 		} else {
             $filter->getQuery()->asc('column_position')->asc(TaskModel::TABLE.'.position');
         }
